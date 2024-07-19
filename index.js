b70c667dfe35d2f3285801a090bd8c43b8336000
jenishh7 <jenishbhilavala.com>
2024-07-19 13:13:39 +0530

    Basic changes

--- index.js
+++ index.js
@@ -1,43 +1,24 @@
 const express = require("express");
-const http = require("http");
-const socketIo = require("socket.io");
-const path = require("path");
 const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
 
-app.use(express.static(path.join(__dirname, "public")));
-
+const { createServer } = require("node:http");
+const server = createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+server.listen(3030, () => {
+  console.log("server running on port 3030");
+});
 io.on("connection", (socket) => {
-  socket.on("join room", ({ username, room }) => {
-    socket.join(room);
-    socket.to(room).emit("user joined", { username, id: socket.id });
-  });
-
-  socket.on("chat message", ({ username, room, message }) => {
-    io.to(room).emit("chat message", { username, message });
-  });
-
-  socket.on("signal", (data) => {
-    io.to(data.userToSignal).emit("signal", {
-      callerID: data.callerID,
-      signal: data.signal
-    });
-  });
-
-  socket.on("returning signal", (data) => {
-    io.to(data.callerID).emit("receiving returned signal", {
-      id: socket.id,
-      signal: data.signal
-    });
-  });
+  console.log("a user connected");
 
   socket.on("disconnect", () => {
-    io.emit("user disconnected", socket.id);
+    console.log("user disconnected");
   });
-});
-
-const PORT = 3030;
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+    io.emit("chat message", msg);
+  });
 });
